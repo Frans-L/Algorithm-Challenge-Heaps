91c98cf3a5b3a545b6a000690d7359360a9763b6
Frans L <jonkun.muun@hotmail.com>
2019-12-26 18:45:41 +0200

    add merge example

--- visual_example.py
+++ visual_example.py
@@ -12,15 +12,15 @@ from visualize import visualize
 # > pip install -r visual_requirements.txt
 
 
-def example_run(heap, title):
+def example_run(heap_type, title):
+    heap = heap_type()
 
     nodes = []
     values = [22, 16, 8, 13, 4, 12, 6, 5, 11]
     for n in values:
         node = heap.insert(n)
         nodes.append(node)
-
-    visualize(heap, title=title + ": 1. Inserted values")
+    visualize(heap, title=title + ": 1. Inserted values - " + str(values))
 
     heap.delete_min()
     visualize(heap, title=title + ": 2. Deleted the minimum node (node 4)")
@@ -37,10 +37,17 @@ def example_run(heap, title):
     heap.delete(nodes[0])
     visualize(heap, title=title + ": 6. Deleted the non-minumum node 10")
 
+    heap2 = heap_type()
+    values = [32, 10, 15]
+    for n in values:
+        node = heap2.insert(n)
+    heap.merge(heap2)
+    visualize(heap, title=title + ": 7. Merged with another heap - " + str(values))
+
 
 if __name__ == "__main__":
     print("--- Running FibonacciHeap ---")
-    example_run(FibonacciHeap(), "Fibonacci")
+    example_run(FibonacciHeap, "Fibonacci")
 
     print("--- Running HollowHeap ---")
-    example_run(HollowHeap(), "Hollow")
+    example_run(HollowHeap, "Hollow")
